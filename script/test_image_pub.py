b3ae041294885ea28a02c3a40f823f4a57ceff7c
yuzoo0226 <fryuzoo@gmail.com>
2022-11-09 13:55:51 +0900

    動作検証用に一つの画像をpubしつづけるノードを作成

---
+++ script/test_image_pub.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import cv2
+import rospy
+import sys
+
+import roslib
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+sys.path.append(roslib.packages.get_pkg_dir("semantic_map") + "/include/omni3d/")
+
+
+class TestImagePubliher():
+    def __init__(self) -> None:
+        image_path = roslib.packages.get_pkg_dir("semantic_map") + "/include/omni3d/datasets/hma/chair.jpg"
+        self.test_image = cv2.imread(image_path, 1)
+        self.pub_image = rospy.Publisher("/image/test", Image, queue_size=10)
+        self.bridge = CvBridge()
+
+        self.smi = self.bridge.cv2_to_imgmsg(self.test_image, encoding="bgr8")
+
+    def publish_image(self):
+        self.pub_image.publish(self.smi)
+
+
+if __name__ == "__main__":
+    rospy.init_node("test_image_publisher")
+    im_pub = TestImagePubliher()
+    rate = rospy.Rate(30)
+
+    while not rospy.is_shutdown():
+        im_pub.publish_image()
+        rate.sleep
+
+
